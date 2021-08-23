b2f670719c349dc433922de141b8bed3b4d7a12f
cpesar <77510555+cpesar@users.noreply.github.com>
2021-08-23 17:24:01 -0600

    navbar rough draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+// Components
+import About from './components/About';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+
+// Pages
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
+import Resume from './pages/Resume';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <Navbar />
+      <Switch>
+        <Route component={Home} path='/' exact />
+        <Route component={About} path='/about' />
+       
+        <Route component={Contact} path='/contact' />
+        <Route component={Projects} path='/projects' />
+        <Route component={Resume} path='/resume' />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+// export default function NavBar(){
+//   return(
+//     <header className='bg-red-600'>
+//       <div className='container mx-auto flex justify-between'>
+//         <nav className='flex'>
+//           <NavLink 
+//           to='/' 
+//           exact 
+//           activeClassName='text-white'
+//           className='inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl '
+//           >
+//             Pesar
+//           </NavLink>
+
+//           <NavLink 
+//           to='/contact' 
+//           activeClassName='text-red-100 bg-red-700'
+//           className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800' 
+//           >
+//           Contact
+//           </NavLink>
+
+//           <NavLink 
+//           to='/projects' 
+//           activeClassName='text-red-100 bg-red-700'
+//           className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800'
+//           >
+//             Projects
+//           </NavLink>
+
+//           <NavLink 
+//           to='/resume' 
+//           activeClassName='text-red-100 bg-red-700'
+//           className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800'
+//           >
+//             Resume
+//           </NavLink>
+
+          
+//         </nav>
+//       </div>
+//     </header>
+//   )
+// }
+
+export default function NavBar(){
+  return(
+    <header className='bg-blue-500'>
+      <div className='container mx-auto flex justify-between'>
+        <nav className='flex'>
+          <NavLink 
+          to='/' 
+          exact 
+          activeClassName='text-white'
+          className='inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-blue-200 text-4xl '
+          >
+            Pesar
+          </NavLink>
+
+          <NavLink 
+          to='/contact' 
+          activeClassName='text-blue-200 bg-blue-300'
+          className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-blue-800' 
+          >
+          Contact
+          </NavLink>
+
+          <NavLink 
+          to='/projects' 
+          activeClassName='text-blue-200 bg-blue-300'
+          className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-blue-800'
+          >
+            Projects
+          </NavLink>
+
+          <NavLink 
+          to='/resume' 
+          activeClassName='text-blue-200 bg-blue-300'
+          className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-blue-800'
+          >
+            Resume
+          </NavLink>
+
+          
+        </nav>
+      </div>
+    </header>
+  )
+}
