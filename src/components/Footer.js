905961acaceb5433fc9490bbb4bd0e39eb8acf77
cpesar <77510555+cpesar@users.noreply.github.com>
2021-08-23 18:51:03 -0600

    added button icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,56 @@
 import React from 'react';
-import { Center, Link } from '@chakra-ui/react'
+import { Center } from '@chakra-ui/react'
 
-import { GitHub, LinkedIn } from '@material-ui/icons';
+import { SocialIcon } from 'react-social-icons';
+// import { GitHub, LinkedIn } from '@material-ui/icons';
+
+// export default function Footer(){
+//   return(
+ 
+//     <div className='inline-flex py-3 px-3 my-6 justify-content space-between'>
+//       <SocialIcon 
+//         url='https://github.com/cpesar' 
+//         className='br-4' 
+//         target='_blank' 
+//         fgColor='#fff' 
+//         style={{ height:50, width:50 }} 
+//         />
+      
+//       <SocialIcon 
+//         url='https://www.linkedin.com/in/chris-pesar-ba07b182/' 
+//         className='br-4' 
+//         target='_blank' 
+//         fgColor='#fff' 
+//         style={{ height:50, width:50 }} />
+//     </div>
+
+
+//   )
+// }
 
 export default function Footer(){
   return(
-    <Center d='flex' h='100px' p='5'>
-     
-
-      <Link href='https://github.com/cpesar' isExternal>
-        <GitHub className='icon'/>
-      </Link>
-
-      <Link href='https://www.linkedin.com/in/chris-pesar-ba07b182/' isExternal>
-        <LinkedIn className='icon'/>
-      </Link>
-
+  
+    <Center d='flex' h='100px' p='5' margin='10px' padding='10px'>
       
-    </Center>
+      <SocialIcon 
+        url='https://github.com/cpesar' 
+        className='br-4' 
+        target='_blank' 
+        fgColor='#fff' 
+        style={{ height:60, width:60 }} 
+        />
+      
+      <SocialIcon 
+        url='https://www.linkedin.com/in/chris-pesar-ba07b182/' 
+        className='br-4' 
+        target='_blank' 
+        fgColor='#fff' 
+        style={{ height:60, width:60 }} 
+        />
+      
+      </Center>
+    
   )
 }
 
