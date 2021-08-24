86b06bbeaf5c4944d17581b324214f771485c359
cpesar <77510555+cpesar@users.noreply.github.com>
2021-08-23 18:09:03 -0600

    added icons to footer rough draft

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,43 @@
 import React from 'react';
+import { Center, Link } from '@chakra-ui/react'
+
+import { GitHub, LinkedIn } from '@material-ui/icons';
 
 export default function Footer(){
-  return <h1>Footer Page</h1>
+  return(
+    <Center d='flex' h='100px' p='5'>
+     
+
+      <Link href='https://github.com/cpesar' isExternal>
+        <GitHub className='icon'/>
+      </Link>
+
+      <Link href='https://www.linkedin.com/in/chris-pesar-ba07b182/' isExternal>
+        <LinkedIn className='icon'/>
+      </Link>
+
+      
+    </Center>
+  )
 }
+
+// export default function Footer(){
+//   return (
+// <div className='footer'>
+//   <div className='itemContainer'>
+//   <GitHub className="icon"/>
+//   <span>cpesar</span>
+// </div>
+
+// <div className='itemContainer'>
+//   <LinkedIn className="icon" />
+//   <span>chrispesar1@gmail.com</span>
+// </div>
+
+// <div className='itemContainer'>
+//   <Email className="icon" />
+//   <span>chrispesar1@gmail.com</span>
+// </div>
+// </div>
+//   )
+// }
