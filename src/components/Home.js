b79ac4fa48c65ef7b59d4a5f6055b329ab419e77
cpesar <77510555+cpesar@users.noreply.github.com>
2021-08-28 13:57:00 -0600

    fixed spacing on home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import mountains from '../assets/mtns-at-night.jpeg';
+import { Link } from 'react-router-dom';
+// import mountains from '../assets/mtns-at-night.jpeg';
 
 import { init } from 'ityped';
 import { useEffect, useRef } from 'react';
@@ -16,21 +17,42 @@ export default function Home(){
     });
   },[]);
 
-  return(
-    <main>
-      <img 
-        src={mountains} 
-        alt='mountains at night' 
-        className='absolute object-cover w-full h-full' 
-      />
+  // return(
+  //   <main>
+  //     <img 
+  //       src={mountains} 
+  //       alt='mountains at night' 
+  //       className='absolute object-cover w-full h-full' 
+  //     />
+
+  //     <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
+  //       <h2 className='lg:text-6xl md:text-5xl sm:text-3xl text-3xl text-blue-500 font-mono'> Aloha I'm Chris </h2>
+  //       <h3 className='lg:text-6xl md:text-5xl sm:text-3xl text-3xl text-blue-200 font-mono justify-content space-between'>
+  //         <span className="justify-center" ref={textRef}> </span>
+  //       </h3>
+  //     </section>
       
-      <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
-        <h2 className='lg:text-6xl md:text-5xl sm:text-3xl text-3xl text-blue-500 font-mono'> Aloha I'm Chris </h2>
-        <h3 className='lg:text-6xl md:text-5xl sm:text-3xl text-3xl text-blue-200 font-mono justify-content space-between'>
-          <span className="justify-center" ref={textRef}> </span>
-        </h3>
-      </section>
-      
-    </main>
-  )
+  //   </main>
+  // )
+
+  return <>
+  <section className="Home">
+    <div className="overlay flex flex-col items-center justify-center text-white px-5">
+      <h1 className="text-5xl font-mono text-right mb-5">
+        Aloha! I'm Chris <span className="text-blue-500" ref={textRef}> </span> 
+      </h1>
+
+      <div className="items-center">
+        <Link to="/contact" 
+          className="mr-5 bg-blue-500 py-2 px-4 rounded-full font-mono hover:bg-blue-800">
+            Contact
+        </Link>
+        <Link to="/projects" 
+          className="mr-5 bg-white text-blue-500 py-2 px-4 rounded-full font-mono hover:bg-blue-800 hover:text-white">
+            Projects
+        </Link>
+      </div>
+    </div>
+  </section>
+</>
 }
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,23 +1,21 @@
-// import React from 'react';
 
-// export default function Resume(){
-//   return <h1>Resume Page</h1>
-// }
 
 import React from 'react'
 // import {Link} from 'react-router-dom';
-import mountains from '../assets/mtns-at-night.jpeg';
+// import mountains from '../assets/mtns-at-night.jpeg';
 
 const Resume = () => {
   return (
     <main>
-    <img 
+    {/* <img 
       src={mountains} 
       alt='mountains at night' 
       className='absolute object-cover w-full h-full' 
-    />
+    /> */}
     <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
-      <h2 className='lg:text-6xl md:text-5xl sm:text-3xl text-3xl text-blue-500 font-mono'> The Resume Page </h2>
+      <h2 className='lg:text-6xl md:text-5xl sm:text-3xl text-3xl text-blue-500 font-mono'> 
+      The Resume Page 
+      </h2>
       
     </section>
   </main>
