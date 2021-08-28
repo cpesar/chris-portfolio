2579f8f5c9f6e8f290277aec819696ef5ec02661
cpesar <77510555+cpesar@users.noreply.github.com>
2021-08-28 15:42:16 -0600

    footer still not rendering on projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,9 +5,11 @@ const Projects = () => {
   const [items, setItems] = useState(ProjectFiles)
 
 
+
+
   return <>
     <section 
-      className="py-20 px-5 bg-green-300 grid grid-col-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 lg:h-screen lg:">
+      className="py-20 px-5 bg-green-300 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 lg:h-screen lg:pt-32">
       {items.map((item) => {
         const {id, path, desc}=item
         return(
